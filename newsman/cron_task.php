66a4c95e9756623eeabd03b393260fbcb32cda93
Victor Dramba <victor.dramba@gmail.com>
2015-10-29 15:41:01 +0200

    coding standards

--- newsman/cron_task.php
+++ newsman/cron_task.php
@@ -1,25 +1,26 @@
 <?php
 /**
- * Copyright 2015 Dazoot Software
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *   @copyright 2015 Dazoot Software
- *   @author Dramba Victor for Newsman
- *   @license http://www.apache.org/licenses/LICENSE-2.0
- */
+Copyright 2015 Dazoot Software
+
+NOTICE OF LICENSE
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ 
+    @author Dramba Victor for Newsman
+    @copyright 2015 Dazoot Software
+    @license http://www.apache.org/licenses/LICENSE-2.0
+
+*/
 
 require_once(dirname(__FILE__).'/../../config/config.inc.php');
 require_once(dirname(__FILE__) . '/newsman.php');
